7c4c8933912191d97c0f4bd810c8851d853eac4a
EmreCanTurgut <emrecanturgut3@gmail.com>
2024-10-22 19:57:33 +0300

    x/ o dynmc gameboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
+import { useState } from "react";
 import Player from "./components/player";
 import GameBoard from "./components/GameBoard";
 
 function App() {
+  const [activePlayer,SetActivePlayer]=useState('X');
+  function handleSelectSquare(){
+    SetActivePlayer((currentActivePlayer)=>currentActivePlayer==='X'?'O':'X');
+  }
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
+        <ol id="players" className="highlight-player">
           <Player
-           initialName={'Player1'} symbol={'X'}/>
-          <Player initialName={'Player2'} symbol={'O'}/>
+           initialName={'Player1'} symbol={'X'} isActive={activePlayer ==='X'} />
+          <Player initialName={'Player2'} symbol={'O'} isActive={activePlayer==='O'}/>
         </ol>   
-        <GameBoard/>
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
       </div>
     </main>
   );
